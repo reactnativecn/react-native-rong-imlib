2362c8bf960d2a69c08904735fab39a34bcb15f3
tdzl2003 <tdzl2003@gmail.com>
2016-11-01 10:41:36 +0800

    update SDK

--- ios/RongCloudSDK/RongIMLib.framework/Headers/RCConversation.h
+++ ios/RongCloudSDK/RongIMLib.framework/Headers/RCConversation.h
@@ -10,12 +10,12 @@
 //  RCConversation.h
 //  Created by Heq.Shinoda on 14-6-13.
 
-#import <Foundation/Foundation.h>
 #import "RCMessageContent.h"
+#import <Foundation/Foundation.h>
 
 /*!
  会话类
- 
+
  @discussion 会话类，包含会话的所有属性。
  */
 @interface RCConversation : NSObject <NSCoding>
@@ -42,8 +42,9 @@
 
 /*!
  是否置顶，默认值为NO
- 
- @discussion 如果设置了置顶，在IMKit的RCConversationListViewController中会将此会话置顶显示。
+
+ @discussion
+ 如果设置了置顶，在IMKit的RCConversationListViewController中会将此会话置顶显示。
  */
 @property(nonatomic, assign) BOOL isTop;
 
@@ -84,10 +85,11 @@
 
 /*!
  会话中最后一条消息的发送者的用户名（已废弃，请勿使用）
- 
+
  @warning **已废弃，请勿使用。**
  */
-@property(nonatomic, strong) __deprecated_msg("已废弃，请勿使用。") NSString *senderUserName;
+@property(nonatomic, strong) NSString *senderUserName
+__deprecated_msg("已废弃，请勿使用。");
 
 /*!
  会话中最后一条消息的消息ID
@@ -101,25 +103,35 @@
 
 /*!
  会话中最后一条消息的json Dictionary
+ 
+ @discussion 此字段存放最后一条消息内容中未编码的json数据。
+ SDK内置的消息，如果消息解码失败，默认会将消息的内容存放到此字段；如果编码和解码正常，此字段会置为nil。
  */
 @property(nonatomic, strong) NSDictionary *jsonDict;
 
 /*!
  最后一条消息的全局唯一ID
- 
+
  @discussion 服务器消息唯一ID（在同一个Appkey下全局唯一）
  */
 @property(nonatomic, strong) NSString *lastestMessageUId;
 
 /*!
- RCConversation初始化方法（已废弃，请勿使用）
+ 会话中是否存在被@的消息
  
+ @discussion 在清除会话未读数（clearMessagesUnreadStatus:targetId:）的时候，会将此状态置成 NO。
+ */
+@property(nonatomic, assign) BOOL hasUnreadMentioned;
+
+/*!
+ RCConversation初始化方法（已废弃，请勿使用）
+
  @param json    会话的json Dictionary
  @return        会话对象
- 
+
  @warning **已废弃，请勿使用。**
  */
 + (instancetype)conversationWithProperties:(NSDictionary *)json
-__deprecated_msg("已废弃，请勿使用。");
+    __deprecated_msg("已废弃，请勿使用。");
 
 @end
